7c2b2a53d401f18a6c80bd7eeeaeb16e78b9c90e
Anton Isakov <anton_isakov@epam.com>
2018-03-08 16:49:19 +0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+const extractSass = new ExtractTextPlugin({
+	filename: "style.css",
+	disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+	watch: true,
+	entry: './src/js/app.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+
+	// resolve: {
+  //   extensions: ['.html', '.js'/*, '.ts'*/]
+  // },
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: extractSass.extract({
+					fallback: "style-loader",
+					use: [
+						{
+							loader: "css-loader", options: {
+								//sourceMap: true
+							}
+						},
+						{
+							loader: "postcss-loader",
+							options: {
+								//sourceMap: true
+							}
+						},
+						{
+							loader: "sass-loader", options: {
+								//sourceMap: true
+							}
+						}
+					]
+				})
+			},
+			{
+				//IMAGE LOADER
+				test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|json)$/i,
+				loader:'file-loader'
+			},
+			{
+        test: /\.html$/,
+				loader: 'html-loader',
+				options: {
+					minimize: process.env.NODE_ENV === "production" ? true : false,
+					//collapseWhitespace: true
+				}
+      }
+		]
+	},
+	plugins: [
+		//new UglifyJsPlugin(),
+		extractSass,
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+			inject: true,
+			cache: true
+    })
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		port: 8081
+	}
+};
