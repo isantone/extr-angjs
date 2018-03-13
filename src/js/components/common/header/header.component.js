397d60c09ba064e717e9633a856124790d511910
Anton_Isakov <anton_isakov@epam.com>
2018-03-13 18:19:43 +0400

    Sync

--- src/js/components/common/header/header.component.js
+++ src/js/components/common/header/header.component.js
@@ -6,12 +6,23 @@ import headerTemplate from './header.html';
 class Header {
   constructor() {
     this.name = 'Header';
+    this.submenuContainer = document.getElementById("submenuContainer");
+
     console.log("Header Here!");
+  }
+
+  showMenu(event) {
+    event.preventDefault();
+
+    console.log('showMenu Working');
+
+		//this.submenuContainer.classList.remove("hide");
   }
 }
 
 export const appHeader = {
   controller: Header,
+  controllerAs: 'HeaderCtrl',
   template: headerTemplate,
 };
 
