d52918c763a88062e6834ca92edf81c966ddc212
Anton_Isakov <anton_isakov@epam.com>
2018-03-13 17:43:48 +0400

    Header added

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,15 @@
 import angular from 'angular';
 
+import '../scss/main.scss';
+
 /// COMPONENTS ///
 import {appFooter} from './components/common/footer/footer.component.js';
+import {appHeader} from './components/common/header/header.component.js';
 /////////////////
 
 
 
 angular.module("ExtremeShop", [/*"router"*/])
+  .component("appHeader", appHeader)
   .component("appFooter", appFooter)
 ;
