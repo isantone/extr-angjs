066404cb1c266a7859ce2ab27f4e03154bfeec1b
Anton Isakov <isakovanton@mail.ru>
2018-04-03 18:16:04 +0400

    Indent fixed

--- src/js/services/es-http-error-interceptor.js
+++ src/js/services/es-http-error-interceptor.js
@@ -1,20 +1,20 @@
 export default function httpErrorInterceptor($q) {
-    const interceptor = {
-        "requestError": (rejection) => {
-            let errorMessage = `Request error. Http status: ${rejection.status} ${rejection.statusText}`;
+  const interceptor = {
+    "requestError": (rejection) => {
+      let errorMessage = `Request error. Http status: ${rejection.status} ${rejection.statusText}`;
 
-            return $q.reject(errorMessage);
-        },
-        "responseError": (rejection) => {
-            let errorMessage = `Response error. Http status: ${rejection.status} ${rejection.statusText}`;
+      return $q.reject(errorMessage);
+    },
+    "responseError": (rejection) => {
+      let errorMessage = `Response error. Http status: ${rejection.status} ${rejection.statusText}`;
 
-            if (rejection.status < 0) {
-                errorMessage = `Response error. Couldn't connect to the database. Http status: ${rejection.status}`;
-            }
+      if (rejection.status < 0) {
+        errorMessage = `Response error. Couldn't connect to the database. Http status: ${rejection.status}`;
+      }
 
-            return $q.reject(errorMessage);
-        }
+      return $q.reject(errorMessage);
     }
+  }
 
-    return interceptor;
+  return interceptor;
 }
